c45b1b95a2125750599b98364a74db2d392de0bb
Jayant1011 <134696481+Jayant1011@users.noreply.github.com>
2024-02-21 22:30:07 +0530

    Add files via upload

---
+++ notebook.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Data Science Notebook
+
+
+# In[ ]:
+
+
+## Introduction
+
+Welcome to this Data Science notebook! In this notebook, we will explore various concepts and tools related to data science, including programming languages, libraries, and tools commonly used in the field.
+
+
+# In[ ]:
+
+
+## Data Science Languages
+
+- Python
+- R
+- SQL
+
+
+# In[ ]:
+
+
+## Data Science Libraries
+
+- Pandas
+- NumPy
+- Scikit-learn
+- Matplotlib
+- Seaborn
+
+
+# In[ ]:
+
+
+## Data Science Tools
+
+| Tool         | Description                      |
+|--------------|----------------------------------|
+| Jupyter      | Interactive computing            |
+| TensorFlow   | Deep learning framework          |
+| Spark        | Distributed computing framework  |
+| Tableau      | Data visualization tool          |
+
+
+# In[ ]:
+
+
+## Arithmetic Expression Examples
+
+Let's explore some basic arithmetic operations using Python.
+
+
+# In[ ]:
+
+
+# Multiplication
+result_multiply = 5 * 3
+print("Multiplication Result:", result_multiply)
+
+# Addition
+result_add = 5 + 3
+print("Addition Result:", result_add)
+
+
+# In[ ]:
+
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+print("120 minutes is equal to", hours, "hours")
+
+
+# In[ ]:
+
+
+## Objectives
+
+- Learn about data science languages and libraries
+- Understand basic arithmetic operations in Python
+- Perform simple conversions and calculations
+
+
+# In[ ]:
+
+
+## Author
+
+- John Doe
+
